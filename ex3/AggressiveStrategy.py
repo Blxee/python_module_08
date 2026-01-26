4699847f185ee06cf2bf532195f19312d42e101e
Blxee <aminedev57@gmail.com>
2026-01-26 11:28:30 +0100

    Deleted: trash from wrong remote

--- ex3/AggressiveStrategy.py
+++
@@ -1,9 +0,0 @@
-from ex3.GameStrategy import GameStrategy
-
-
-class AggressiveStrategy(GameStrategy):
-    def execute_turn(self, hand: list, battlefield: list) -> dict: ...
-
-    def get_strategy_name(self) -> str: ...
-
-    def prioritize_targets(self, available_targets: list) -> list: ...
